c6f3c4e64b42701b92b824acb0dc921a5f555972
vancanhuit <vancanhuit@gmail.com>
2022-09-04 06:54:24 +0700

    Refactor test

--- src/app/test_main.py
+++ src/app/test_main.py
@@ -2,25 +2,24 @@ from fastapi.testclient import TestClient
 
 from app.main import app
 
-client = TestClient(app)
-
 
 def test_main():
-    url = "https://reddit.com"
-    response = client.post(
-        "/api/shorten",
-        headers={"Content-Type": "application/json"},
-        json={"url": url},
-    )
+    with TestClient(app) as client:
+        url = "https://reddit.com"
+        response = client.post(
+            "/api/shorten",
+            headers={"Content-Type": "application/json"},
+            json={"url": url},
+        )
 
-    assert response.status_code == 200
-    assert "short_link" in response.json()
-    short_link = response.json()["short_link"]
-    assert len(short_link) == 7
+        assert response.status_code == 200
+        assert "short_link" in response.json()
+        short_link = response.json()["short_link"]
+        assert len(short_link) == 7
 
-    response = client.get("/{}".format(short_link), allow_redirects=False)
-    assert response.status_code == 307
-    assert response.headers["Location"] == url
+        response = client.get("/{}".format(short_link), allow_redirects=False)
+        assert response.status_code == 307
+        assert response.headers["Location"] == url
 
-    response = client.get("/non-existing-url", allow_redirects=False)
-    assert response.status_code == 404
+        response = client.get("/non-existing-path", allow_redirects=False)
+        assert response.status_code == 404
